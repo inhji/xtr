d507300a8f819eab26b41a9e394d6c60980e38de
Jonathan Jenne <jjenne@inhji.com>
2014-01-05 04:15:23 +0100

    add parsePackageJson

--- index.js
+++ index.js
@@ -4,15 +4,17 @@
 var child   = require("child_process");
 var _       = require("underscore");
 var colors  = require("colors");
-
-var createDirName = require("./lib/createDirName.js");
-var parseFileExtension = require("./lib/parseFileExtension.js");
+var fs      = require('fs');
 
 // Plugins
 var tar     = require("./plugins/tar.js");
 var bz2     = require("./plugins/bz2.js");
 var zip     = require("./plugins/zip.js");
 var z7      = require("./plugins/7z.js");
+
+// Lib
+var createDirName       = require("./lib/createDirName.js");
+var parseFileExtension  = require("./lib/parseFileExtension.js");
 
 // Types
 var types = [
@@ -28,41 +30,72 @@ var types = [
 var filename = process.argv.slice(2)[0];
 var type = parseFileExtension(filename, types);
 
+var parsePackageJson = function (callback) {
+    var file = __dirname + '/package.json';
+
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            callback(err);
+        }
+     
+        data = JSON.parse(data);
+        //console.log(data);
+        callback(null, data);
+    });
+}
+
 // Show help & exit
-var showHelp = function () {
-    console.log('XTR'.bold + ' - ' + 'extract archives (tar, tgz, bz2, zip, 7z)'.rainbow);
+if ( !filename || filename === "-h" || filename === "--help" ) {
+    parsePackageJson(function (err, data) {
+        if (err) {
+            console.log('Cannot read package.json:'.red.bold);
+            console.log('     Error Number: '.red.bold + err.errno.toString().red);
+            console.log('     Exitcode: '.red.bold + err.code.red);
+            console.log('     Path: '.red.bold + err.path.red);
+            process.exit(1);
+        };
+        console.log('XTR'.green.bold + ' - '.green + 'extract archives (tar, tgz, bz2, zip, 7z)'.green);
         console.log('');
-        console.log('  Usage'.bold + ': xtr [options] [filename]');
+        console.log('   Version: '.green.bold);
+        console.log('       ' + data.version.green);
+        console.log('');
+        console.log('   Usage:'.green.bold);
+        console.log('       xtr [options] [filename]'.green);
         console.log('  ');
-        console.log('    Options:');
-        console.log('  ');
-        console.log('      -h, --help     output usage information');
-        console.log('      -V, --version  output the version number');
+        console.log('   Options:'.green.bold);
+        console.log('       -h, --help     output usage information'.green);
+        console.log('       -v, --version  output the version number'.green);
         console.log('');
-        console.log('  What it does:'.bold);
+        console.log('   What it does:'.green.bold);
+        console.log('       Extracts the file extensions listed below'.green);
+        console.log('       to a directory with the following filename:'.green);
+        console.log('       <filename>_<DD-MM-JJJJ>_<timestamp>'.green);
         console.log('');
-        console.log('    Extracts the file extensions listed below');
-        console.log('    to a directory with the following filename:');
-        console.log('    <filename>_<DD-MM-JJJJ>_<timestamp>');
-        console.log('');
-        console.log('  Supported filetypes:'.bold);
-        console.log('');
-        console.log('    .tar');
-        console.log('    .tar.gz');
-        console.log('    .tar.bz2');
-        console.log('    .zip');
-        console.log('    .7z');
-        console.log('');
-}
-
-if (!filename) {
-    showHelp();
-    process.exit(0);
-}
-
-type.fn(filename, type), function (err, res) {
-    if (err) {
-        console.log(err.red);
-    };
-    console.log("File ".green + filename.green.bold + " successfully extracted to ".green + type.dir.green.bold + ".".green);
+        console.log('   Supported filetypes:'.green.bold);
+        console.log('       .tar'.green);
+        console.log('       .tar.gz'.green);
+        console.log('       .tar.bz2'.green);
+        console.log('       .zip'.green);
+        console.log('       .7z'.green);
+        process.exit(0);
+    });
+// Show version & exit
+} else if( filename === "-v" || filename === "--version" ) {
+    parsePackageJson(function (err, data) {
+        if (err) {
+            process.exit(1);
+        };
+        console.log(data.version);
+        process.exit(0);
+    });
+// Extract & show success message
+} else {
+    // Call extract function
+    // params: filename, type
+    type.fn(filename, type, function (err, res) {
+        if (err)
+            console.log(err.red);
+        var msg = "Directory created: " + type.dir.bold + "(" + type.ext + ")";
+        console.log(msg.green);
+    });
 }
