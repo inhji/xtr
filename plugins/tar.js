51f90747c159a7b2e8566e2c40c71ace8773766d
Jonathan Jenne <jjenne@inhji.com>
2014-01-05 02:32:45 +0100

    remove createDirName from tar.js

--- plugins/tar.js
+++ plugins/tar.js
@@ -1,9 +1,8 @@
 var tarball = require("tarball-extract");
-var createDirName = require("../lib/createDirName.js");
 
 // I extract a Tar File! 
 var extractTar = function (file, type, callback) {
-    tarball.extractTarball(file, createDirName(type.dir), function(err){
+    tarball.extractTarball(file, type.dir, function(err){
         if(err) callback(err);
         callback(null, true);
     });
